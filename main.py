59d1692c43c5ea6195166c750a8cd570c0d178db
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-04-06 19:42:38 +0900

    add : サンプルスクリプトを作成した

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 
 # プロキシが必要なら以下を追加
@@ -9,9 +10,16 @@ proxies = {
 }
 ## END
 
+# データのロード
 url = "https://www.statlab.co.jp/seminar/winequality-red2.csv"
 
 urlData = requests.get(url, proxies=proxies).content
 
+## いったん保存
 with open("winequality-red2.csv", "wb") as f:
     f.write(urlData)
+
+## データフレームとして読み込む
+df = pd.read_csv("winequality-red2.csv")
+
+# 前処理
